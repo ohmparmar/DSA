9218ffc07fd4b28f464467a7a1e4cbbc301c4efe
Ohm Parmar <ohm3658@gmail.com>
2025-03-23 22:41:15 +0530

    minor changes in bubble sort

--- algorithms/bubble_sort.js
+++ algorithms/bubble_sort.js
@@ -7,9 +7,7 @@ function bubbleSort(array) {
         let swapped = false;
         for (let j = 0; j < length - i - 1; j++) {
             if (array[j] > array[j + 1]) {
-                let temp = array[j]
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
+                [array[j], array[j + 1]] = [array[j + 1], array[j]]
                 swapped = true
             }
         }
