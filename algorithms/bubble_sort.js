17e73a3ab0d119ffa67162a865a076c126b7715d
Ohm Parmar <ohm3658@gmail.com>
2025-03-23 21:43:50 +0530

    optimization in bubble sort

--- algorithms/bubble_sort.js
+++ algorithms/bubble_sort.js
@@ -2,14 +2,18 @@ const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
 function bubbleSort(array) {
     const length = array.length;
+
     for (let i = 0; i < length; i++) {
-        for (let j = 0; j < length; j++) {
+        let swapped = false;
+        for (let j = 0; j < length - i - 1; j++) {
             if (array[j] > array[j + 1]) {
                 let temp = array[j]
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
+                swapped = true
             }
         }
+        if (!swapped) break;
     }
 }
 
