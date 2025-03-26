4792cc658281eb958d2473c970cc8c42d990ba66
Ohm Parmar <ohm3658@gmail.com>
2025-03-26 22:14:39 +0530

    insertion sort

---
+++ algorithms/insertion_sort.js
@@ -0,0 +1,18 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+
+function insertionSort(array) {
+    for (let i = 1; i < array.length; i++) {
+        let temp = array[i];
+        let j = i - 1;
+
+        while (j >= 0 && array[j] > temp) {
+            array[j + 1] = array[j];
+            j--;
+        }
+        array[j + 1] = temp;
+    }
+
+    return array;
+}
+
+console.log(insertionSort(numbers));
